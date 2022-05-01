260528ad847aac070db39a74602061d243939e28
Connor Carpenter <connorcarpenter@gmail.com>
2022-05-01 00:18:46 -0500

    re-arranging proxy

---
+++ old_client/src/lib.rs
@@ -0,0 +1,5 @@
+mod socket;
+mod addr_cell;
+
+pub use socket::Socket;
+pub use addr_cell::{ServerAddr, AddrCell};
--- proxy/src/lib.rs
+++ proxy/src/lib.rs
@@ -1,26 +1,26 @@
 use cfg_if::cfg_if;
 
 cfg_if! {
-    if #[cfg(all(feature = "use_webrtc", feature = "use_proxy"))]
+    if #[cfg(all(feature = "use_old", feature = "use_new"))]
     {
-        compile_error!("Can't use 'use_webrtc' AND 'use_proxy' together");
+        compile_error!("Can't use 'use_old' AND 'use_new' together");
     }
-    else if #[cfg(all(not(feature = "use_webrtc"), not(feature = "use_proxy")))]
+    else if #[cfg(all(not(feature = "use_old"), not(feature = "use_new")))]
     {
-        compile_error!("Must use either 'use_webrtc' OR 'use_proxy' feature flag, you must choose one!");
-    }
-}
-
-cfg_if! {
-    if #[cfg(feature = "use_webrtc")]
-    {
-        pub use webrtc::*;
+        compile_error!("Must use either 'use_old' OR 'use_new' feature flag, you must choose one!");
     }
 }
 
 cfg_if! {
-    if #[cfg(feature = "use_proxy")]
+    if #[cfg(feature = "use_old")]
     {
-        pub use webrtc_unreliable_client_slim::*;
+        pub use webrtc_unreliable_client_old::*;
+    }
+}
+
+cfg_if! {
+    if #[cfg(feature = "use_new")]
+    {
+        pub use webrtc_unreliable_client::*;
     }
 }
