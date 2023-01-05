3db715a450af3f88b76469734e453c7265b4e9cd
mvlabat <mvlabat@gmail.com>
2023-01-05 21:26:24 +0200

    Allow loopback interface

--- client/src/webrtc/crates/ice/util/mod.rs
+++ client/src/webrtc/crates/ice/util/mod.rs
@@ -65,9 +65,7 @@ pub(crate) async fn local_interfaces(
 
         for ipnet in iface.addrs() {
             let ipaddr = ipnet.addr();
-            if !ipaddr.is_loopback()
-                && ((ipv4requested && ipaddr.is_ipv4()) || (ipv6requested && ipaddr.is_ipv6()))
-            {
+            if ipv4requested && ipaddr.is_ipv4() || ipv6requested && ipaddr.is_ipv6() {
                 ips.insert(ipaddr);
             }
         }
